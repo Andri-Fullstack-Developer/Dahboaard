83b5a72e0a303b78da61073fc767af91d8481749
Andri <a8461565@gmail.com>
2024-03-16 14:03:46 +0700

    update

--- app/api/category/route.ts
+++
@@ -1,8 +0,0 @@
-// import { PrismaClient } from '@prisma/client';
-
-// const prisma = new PrismaClient();
-
-// export async function GET(req: Request) {
-//     const result = await prisma.categorys.findMany();
-//     return Response.json({ message: 'ok', status: 200, category: result });
-// }
