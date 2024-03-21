3b0c6e5b15d3451a33c2f8a198659418516b5e43
Andri <a8461565@gmail.com>
2024-03-22 03:20:25 +0700

    update data

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['s3.ap-southeast-1.amazonaws.com', 'kkpffydweumgewhvxzow.supabase.co']
-    },
+        domains: ['s3.ap-southeast-1.amazonaws.com', 'kkpffydweumgewhvxzow.supabase.co', 'bootdey.com']
+    }
     // experimental: {
     //     serverActions: true, // Aktifkan Server Actions
     //     appDir: true
     // }
 };
-
 
 module.exports = nextConfig;
